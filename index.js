88559b598c37c4ab024071379cf60cf32130bc27
shady salem <shady.salem307@gmail.com>
2021-08-24 15:31:38 +0300

    updates css/html/js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // javascript
+
+const div = document.getElementById("movie");
+div.addEventListener("click", (event) => {
+  event.preventDefault();
+  const movieName = document.getElementById("text").value;
+
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=9b32cf47ce3b6d0d186ce6db0bc4eedb`
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
